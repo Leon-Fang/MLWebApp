1186efbc5f97a127366205baf2feee755251b30c
Leon-Fang <13022143580@163.com>
2020-09-28 07:46:24 +0800

    udpate

--- mlweb/main/views.py
+++ mlweb/main/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render,HttpResponse
+import pandas as pd
 
 def index(request):
-    return render(request,'AnalysisPage.html')
+    df = pd.read_csv("D:\github\MLWebApp\static\data\GBPUSD_M1.csv",sep='\t')
+    context = {}
+    context['headers'] = df.columns.tolist()
+    context['timestamp'] = df['Time'][:10].values.tolist()
+    context['Open'] = df['Open'][:10].values.tolist()
+    context['High'] = df['High'][:10].values.tolist()
+    context['Low'] = df['Low'][:10].values.tolist()
+    context['Close'] = df['Close'][:10].values.tolist()
+    context['Volume'] = df['Volume'][:10].values.tolist()
+    return render(request,'AnalysisPage.html', context)
+

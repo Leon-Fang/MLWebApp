c60a91f35c0280849befbc9b0891bb35ea74358d
LeonFang <leon.fang@sap.com>
2020-09-29 17:55:50 +0800

    show the image in web ui

--- mlweb/main/views.py
+++ mlweb/main/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render,HttpResponse
 import pandas as pd
 
 def index(request):
-    df = pd.read_csv("D:\github\MLWebApp\static\data\GBPUSD_M1.csv",sep='\t')
+    df = pd.read_csv("../static/data/GBPUSD_M1.csv",sep='\t')
     context = {}
-    context['headers'] = df.columns.tolist()
-    context['timestamp'] = df['Time'][:10].values.tolist()
-    context['Open'] = df['Open'][:10].values.tolist()
-    context['High'] = df['High'][:10].values.tolist()
-    context['Low'] = df['Low'][:10].values.tolist()
-    context['Close'] = df['Close'][:10].values.tolist()
-    context['Volume'] = df['Volume'][:10].values.tolist()
-    return render(request,'AnalysisPage.html', context)
+    context['cols'] = df.columns.tolist()
+    context['timestamp'] = df['Time'][:1000].values.tolist()
+    context['Open'] = df['Open'][:1000].values.tolist()
+    context['High'] = df['High'][:1000].values.tolist()
+    context['Low'] = df['Low'][:1000].values.tolist()
+    context['Close'] = df['Close'][:1000].values.tolist()
+    context['Volume'] = df['Volume'][:1000].div(100).values.tolist()    
 
+    return render(request,'AnalysisPage.html', context)

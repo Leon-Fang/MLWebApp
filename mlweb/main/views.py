5cfae0304829da6d8410f394f4a049522bb8d60c
LeonFang <leon.fang@sap.com>
2020-10-02 18:22:29 +0800

    update image

--- mlweb/main/views.py
+++ mlweb/main/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render,HttpResponse
 import pandas as pd
 
 def index(request):
-    df = pd.read_csv("../static/data/GBPUSD_M1.csv",sep='\t')
+    df = pd.read_csv("../static/data/GBPUSD_M15.csv",sep='\t')
     context = {}
     context['cols'] = df.columns.tolist()
-    context['timestamp'] = df['Time'][:1000].values.tolist()
-    context['Open'] = df['Open'][:1000].values.tolist()
-    context['High'] = df['High'][:1000].values.tolist()
-    context['Low'] = df['Low'][:1000].values.tolist()
-    context['Close'] = df['Close'][:1000].values.tolist()
-    context['Volume'] = df['Volume'][:1000].div(100).values.tolist()    
+    context['timestamp'] = df['Time'][:200].values.tolist()
+    context['Open'] = df['Open'][:200].values.tolist()
+    context['High'] = df['High'][:200].values.tolist()
+    context['Low'] = df['Low'][:200].values.tolist()
+    context['Close'] = df['Close'][:200].values.tolist()
+    context['Volume'] = df['Volume'][:200].div(3000).values.tolist()    
 
     return render(request,'AnalysisPage.html', context)
